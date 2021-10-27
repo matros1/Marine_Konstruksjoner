b02280ce0563fa8db502649230828902a854e7a3
matros1 <92890975+matros1@users.noreply.github.com>
2021-10-27 22:43:45 +0200

    Read file change

--- py_files/readTextToArray.py
+++ py_files/readTextToArray.py
@@ -32,11 +32,11 @@ def readAll():
     Reads nodedata, beamdata, materialdata, nodeloaddata and beamloaddata to np array.
     :return: np array of nodedata, beamdata, materialdata, nodeloaddata and beamloaddata.
     '''
-    NODE = readCSV_float("py_files/NodeData.txt")
+    NODE = readCSV_float("NodeData.txt")
     # x, z, u, w, theta
     # På u,w,theta er 1="Denne er ukjent, må tas med i matrisen", 0="Denne er 0"
 
-    BEAM = readCSV_int("py_files/BeamData.txt")
+    BEAM = readCSV_int("BeamData.txt")
     # N1, N2, M, G
     # N1-Node lengst til venstre
     # N2-Node lengst til høyre
@@ -44,20 +44,20 @@ def readAll():
     #  Geometri. 1 = pipe, 2 = IPE
     #  Geometri utvalgt fra biblioteket
 
-    MATERIAL = readCSV_float("py_files/MaterialData.txt")
+    MATERIAL = readCSV_float("MaterialData.txt")
     # E-modul, flytspenning, tverrkontraksjonstallet
     # 1 er stål og 2 er aluminium
 
-    NODELOAD = readCSV_int("py_files/NodeLoadData.txt")
+    NODELOAD = readCSV_int("NodeLoadData.txt")
     # Nodenr, Fx, Fy, Fz, Mx, My, Mz
 
-    BEAMLOAD = readCSV_int("py_files/BeamLoadData.txt")
+    BEAMLOAD = readCSV_int("BeamLoadData.txt")
 
 
-    PipeData = readCSV_float("py_files/PipeData")
+    PipeData = readCSV_float("PipeData")
     # radius, ratio air
 
-    IPEData = readCSV_float("py_files/IPEData")
+    IPEData = readCSV_float("IPEData")
     #total height, width top, bottom, mid, thickness topp, bot
 
     return NODE,BEAM,MATERIAL,NODELOAD,BEAMLOAD, PipeData, IPEData
